30d48341aabeef15ae5fd648f03ca3a01b7cc509
VilleSchulz <villemsc@metropolia.fi>
2023-08-22 13:01:58 +0300

    Versio 3

---
+++ Moduuli2/Tehtävä 3.py
@@ -0,0 +1,6 @@
+kanta=int(input("Anna suorakulmion kannan pituus:"))
+korkeus=int(input("Anna suorakulmion korkeus:"))
+pinta_ala=kanta * korkeus
+piiri=kanta * 2 + korkeus * 2
+
+print("Suorakulmion pinta-ala on: " + str(pinta_ala)  +  " Ja piiri on: " + str(piiri))
