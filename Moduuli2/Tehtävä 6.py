d4a4de71c83088672719d05ea408a8430e15cb4f
VilleSchulz <villemsc@metropolia.fi>
2023-08-22 14:27:46 +0300

    Versio 3

---
+++ Moduuli2/Tehtävä 6.py
@@ -0,0 +1,17 @@
+import random
+ # Kolmen luvun koodi
+num1_1=random.randint(0,9)
+num2_1=random.randint(0,9)
+num3_1=random.randint(0,9)
+
+# 4 luvun koodi
+num1_2=random.randint(1,6)
+num2_2=random.randint(1,6)
+num3_2=random.randint(1,9)
+num4_2=random.randint(1,6)
+
+
+code1 = str(num1_1) + str(num2_1) + str(num3_1)
+code2 = str(num1_2) + str(num2_2) + str(num3_2) + str(num4_2)
+
+print("Tässä kaksi random avainkoodia:\n" +str(code1) + "\n" +str(code2))
