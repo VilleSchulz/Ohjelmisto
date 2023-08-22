f0afed608870465ef6a1c7a2c0882ee132f91c9f
VilleSchulz <villemsc@metropolia.fi>
2023-08-22 13:22:01 +0300

    Versio 3

---
+++ Moduuli2/Tehtävä 4.py
@@ -0,0 +1,11 @@
+print("Anna kolme kokonaisluku, joista lasken summan, tulon ja keskiarvon")
+nro1 = int(input ("Ensimmäinen kokonaisluku: "))
+nro2 = int(input ("Toinen kokonaisluku: "))
+nro3 = int(input ("Kolmas kokonaisluku: "))
+
+summa= nro1 + nro2 + nro3
+tulo= nro1 * nro2 * nro3
+keskiarvo= (nro1 + nro2 + nro3)/3
+print ("Summa: " + str(summa))
+print ("Tulo: " + str(tulo))
+print ("Keskiarvo: " + str(keskiarvo))
