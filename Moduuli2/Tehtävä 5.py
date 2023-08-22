d363f6e3fee59e81223276bf9082dc107403273c
VilleSchulz <villemsc@metropolia.fi>
2023-08-22 14:00:07 +0300

    Versio 3

---
+++ Moduuli2/Tehtävä 5.py
@@ -0,0 +1,16 @@
+
+import math
+leiviskät = int(input ("Anna leiviskät: "))
+naulat = int(input("Anna naulat: "))
+luoti = int(input("Anna luodit: "))
+
+leiviskä_m = leiviskät * 8512
+naula_m = naulat * 425.6
+luoti_m = luoti * 13.3
+
+kokonaismassa = leiviskä_m + naula_m + luoti_m
+kokonaismassa_KG = kokonaismassa/1000
+result = math.modf(kokonaismassa_KG)
+result_kg =result[1]
+result_g = result[0]
+print(f"Massa nykymittojen mukaan: \n {result_kg:1.0f}  kilogrammaa ja {result_g:1.0f} grammaa.")
