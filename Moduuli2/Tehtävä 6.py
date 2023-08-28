d8ee24f9cbbd7c5bdac16a7f1aa3fdda174110a9
VilleSchulz <villemsc@metropolia.fi>
2023-08-28 19:41:23 +0300

    Versio 3

--- Moduuli2/Tehtävä 6.py
+++ Moduuli2/Tehtävä 6.py
@@ -2,18 +2,8 @@ import random
 print("Paina enter-näppäintä arpoaksesi avainkoodit")
 input()
 # Kolmen luvun koodi
-num1_1=random.randint(0,9)
-num2_1=random.randint(0,9)
-num3_1=random.randint(0,9)
-
+code1 = str(random.randint(0,9)) + str(random.randint(0,9)) + str(random.randint(0,9))
 # 4 luvun koodi
-num1_2=random.randint(1,6)
-num2_2=random.randint(1,6)
-num3_2=random.randint(1,6)
-num4_2=random.randint(1,6)
-
-
-code1 = str(num1_1) + str(num2_1) + str(num3_1)
-code2 = str(num1_2) + str(num2_2) + str(num3_2) + str(num4_2)
+code2 = str(random.randint(1,6)) + str(random.randint(1,6)) + str(random.randint(1,6))+ str(random.randint(1,6))
 
 print("Tässä kaksi random avainkoodia:\n" +str(code1) + "\n" +str(code2))
