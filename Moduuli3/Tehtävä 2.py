931d93e5005b71b7df3eeb4061e96f6929a30139
VilleSchulz <villemsc@metropolia.fi>
2023-08-28 17:36:53 +0300

    Versio 3

--- Moduuli3/Tehtävä 2.py
+++ Moduuli3/Tehtävä 2.py
@@ -1,8 +1,14 @@
-hytti_luokka= str(input("Anna hyttiluokkasi"))
+hytti_luokka= str(input("Anna hyttiluokkasi (LUX,A,B,C) : ")).upper()
+
 if hytti_luokka== "LUX":
     print("Hyttisi on parvekkeellinen hytti yläkannella.")
 
-elif hytti_luokka="A"
+elif hytti_luokka=="A":
         print("Hyttisi on ikkunallinen hytti autokannen yläpuolella.")
+elif hytti_luokka=="B":
+    print("Hyttisi on ikkunaton hytti autokannen yläpuolella")
+elif hytti_luokka=="C":
+    print("Hyttisi on ikkunaton hytti autokannen alapuolella")
 
-    if hytti_luokka: =str(B)
+else:
+    print("Virheellinen hyttiluokka")
