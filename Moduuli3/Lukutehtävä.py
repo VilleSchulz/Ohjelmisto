9a749a126384c931da20ec921197b22e2e9c4b84
VilleSchulz <villemsc@metropolia.fi>
2023-08-28 15:52:55 +0300

    Versio 3

---
+++ Moduuli3/Lukutehtävä.py
@@ -0,0 +1,7 @@
+luku1 = int(input("Anna luku:"))
+if luku1>0:
+    print("luku on positiivinen")
+if luku1==0:
+    print("luku on nolla")
+if luku1<0:
+    print("luku on negatiivinen")
---
+++ Moduuli3/Tehtävä 2.py
@@ -0,0 +1,8 @@
+hytti_luokka= str(input("Anna hyttiluokkasi"))
+if hytti_luokka:= str(LUX)
+    print("Hyttisi on parvekkeellinen hytti yläkannella.")
+else :
+    if hytti_luokka:=str(A)
+        print("Hyttisi on ikkunallinen hytti autokannen yläpuolella.")
+else:
+    if hytti_luokka: =str(B)
