a40bc83a034db4da211f03f6800ca531e698f4c6
VilleSchulz <villemsc@metropolia.fi>
2023-08-29 13:27:15 +0300

    Versio 3

---
+++ Moduuli4/Tehtävä 3.py
@@ -0,0 +1,26 @@
+#Kysyy lukua niin pitkään ennen kuin käyttäjä syöttää tyhjän merkkijonon
+
+
+while True :
+    luku=input('Anna luku:')
+    if luku!="":
+        continue
+    else:
+        break
+
+'''toinen tapa tehdä(itselle muistiin):
+num_list = []
+
+while True:
+    user_input = input("Anna luku: ")
+    if user_input != "":
+        num_list.append(int(user_input))
+        continue
+    else:
+        break
+
+print("Pienin luku: " + str(min(num_list)))
+print("Suurin luku: " + str(max(num_list)))'''
+
+
+
