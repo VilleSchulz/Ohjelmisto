1fd76b2c4cb52c06b0dc3447718cd1ca6caf0ae5
VilleSchulz <villemsc@metropolia.fi>
2023-08-29 15:22:50 +0300

    Versio 3

--- Moduuli4/testi.py
+++ Moduuli4/testi.py
@@ -9,11 +9,17 @@ while tehdyt_haukut<kerrat:
 print('koira haukkuu koko ajan')
 käsky = input('Anna käsky ''stop'' jos haluat että koira lopettaa haukkumisen')
 while käsky !='stop':
+    if käsky == 'saatana':
+        break
     haukku = 0
     while haukku<10:
         print('HAU')
         haukku += 1
     käsky = input('Anna käsky ''stop'' jos haluat että koira lopettaa haukkumisen')
 
+else:
+    print('Ihanaa kun on hiljaista')
+
+print('Ei haukkunu sit')
 
 
