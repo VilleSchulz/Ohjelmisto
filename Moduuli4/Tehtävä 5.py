9147ea9770ad8a0057bf2633ce498657f6f7cdee
VilleSchulz <villemsc@metropolia.fi>
2023-08-29 15:30:14 +0300

    Versio 3

--- Moduuli4/Tehtävä 5.py
+++ Moduuli4/Tehtävä 5.py
@@ -1,6 +1,5 @@
-user = 0
-pword = 0
-while user!='python' and pword!='rules':
+user = pword = 0
+while  user!='python' and pword!='rules':
     user = input("Anna käyttäjätunnus: ")
     pword = input('Anna salanasi: ')
     if user!='python' and pword!='rules':
