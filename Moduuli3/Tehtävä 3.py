9800f26f8540c0113776c349fab929e9c2468b35
VilleSchulz <villemsc@metropolia.fi>
2023-09-04 13:50:46 +0300

    valmis

--- Moduuli3/Tehtävä 3.py
+++ Moduuli3/Tehtävä 3.py
@@ -4,17 +4,17 @@ hemoglobiini= int(input("Anna hemoglobiiniarvosi (g/l): "))
 if sukupuoli=="M":
         if hemoglobiini<134:
             print("Hemoglobiiniarvosi on alhainen, miehen normaali arvo on välillä 134-195g/l")
-        if 134<=hemoglobiini<=195:
+        elif 134<=hemoglobiini<=195:
             print("Hemoglobiinisi on normaalilla tasolla, miehen normaali arvo on välillä 134-195g/l")
-        if hemoglobiini>195:
+        elif hemoglobiini>195:
             print("Hemoglobiinisi on korkealla, miehen normaali arvo on välillä 134-195g/l")
 else:
     if sukupuoli=="N":
         if hemoglobiini<117:
             print("Hemoglobiinisi on alhainen, naisen normaali hemoglobiiniarvo on välillä 117-175 g/l")
-        if 117<=hemoglobiini<=175:
+        elif 117<=hemoglobiini<=175:
             print("Hemoglobiinisi on normaalilla tasolla, naisen normaali arvo on välillä 117-175g/l")
-        if hemoglobiini>175:
+        elif hemoglobiini>175:
             print("Hemoglobiinisi on korkealla, naisen normaali arvo on välillä 117-175g/l")
 
 
