654df35ea8a29924f552e995b3b5f512ed8002b2
VilleSchulz <villemsc@metropolia.fi>
2023-09-04 15:10:24 +0300

    valmis

--- Moduuli4/Tehtävä 3.py
+++ Moduuli4/Tehtävä 3.py
@@ -8,9 +8,26 @@ while True:
     else:
         break
 '''
-luku = 0
-while luku!='':
-    luku = input('Anna luku: ')
+print('Ohjelma tulostaa saaduista luvuista pienimmän ja suurimman')
+print('Jos annat jotain paitsi lukua - ohjelma lopettaa toimintansa \n')
+
+var = input('Anna joku luku: ')
+
+# merkkien jono lataan "[...]"
+merkkijen_jono = []
+
+while True:
+    try:
+        float(var)
+        # append = lisays
+        merkkijen_jono.append(var)
+        var = input('Anna seurava luku: ')
+    except Exception:
+        break
+
+print(f'''
+{min(merkkijen_jono)} - on pienin sinun luvuista
+{max(merkkijen_jono)} - on isoin sinun luvuista ''')
 
 
 '''Joelin tapa tehdä(itselle muistiin ):
