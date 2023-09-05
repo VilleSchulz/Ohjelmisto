a5b6dbdf0c44c60a7e13f544d9547b7526a36063
Ville <villesc@gmail.com>
2023-09-05 15:31:29 +0300

    kesken

--- Moduuli5/Tehtävä1.py
+++ Moduuli5/Tehtävä1.py
@@ -1,9 +1,13 @@
 '''Kirjoita ohjelma, joka kysyy käyttäjältä arpakuutioiden lukumäärän.
 Ohjelma heittää kerran kaikkia arpakuutioita ja tulostaa silmälukujen summan.
 Käytä for-toistorakennetta.'''
-import random
-dicecount = int(input('Anna arpakuutioiden lukumäärä: ')
-nopanluku = random.randint(1,6)
-for i range(dicecount)
 
+import random
+dicecount = int(input('Anna arpakuutioiden lukumäärä: '))
+dicenum_result = []
+for _ in range(dicecount):
+    dicenum = random.randint(1,6)
+    dicenum_result.append(dicenum)
+
+print(f'Silmälukujen summa on: {sum(dicenum_result)}')
 
