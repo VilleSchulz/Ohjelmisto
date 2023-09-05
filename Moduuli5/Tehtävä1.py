077893076a7820a6b3260a7c9e86a510e5f9dfa8
VilleSchulz <villemsc@metropolia.fi>
2023-09-05 11:45:58 +0300

    valmis

---
+++ Moduuli5/Tehtävä1.py
@@ -0,0 +1,9 @@
+'''Kirjoita ohjelma, joka kysyy käyttäjältä arpakuutioiden lukumäärän.
+Ohjelma heittää kerran kaikkia arpakuutioita ja tulostaa silmälukujen summan.
+Käytä for-toistorakennetta.'''
+import random
+dicecount = int(input('Anna arpakuutioiden lukumäärä: ')
+nopanluku = random.randint(1,6)
+for i range(dicecount)
+
+
