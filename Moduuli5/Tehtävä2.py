84471dea2121e001b8365f71b60c29db63a7030d
Ville <villesc@gmail.com>
2023-09-08 13:50:07 +0300

    kommentit päivitetty

--- Moduuli5/Tehtävä2.py
+++ Moduuli5/Tehtävä2.py
@@ -1,6 +1,7 @@
 '''Kirjoita ohjelma, joka kysyy käyttäjältä lukuja siihen saakka, kunnes tämä syöttää tyhjän merkkijonon lopetusmerkiksi.
  Lopuksi ohjelma tulostaa saaduista luvuista viisi suurinta suuruusjärjestyksessä suurimmasta alkaen. Vihje: listan
  alkioiden lajittelujärjestyksen voi kääntää antamalla sort-metodille argumentiksi reverse=True. '''
+'
 
 num_list = []
 
@@ -8,13 +9,31 @@ while True:
     user_input = input("Anna luku: ")
     if user_input == "":
         break
+    elif user_input != "":
+        num = float(user_input) # muutetaan string floatiksi
+        num_list.append(num)
+
+num_list.sort(reverse = True) # Järjestetään nousevaan järjestykseen luvut
+
+result = num_list[:5]
+print(f'Tässä viisi suurinta numeroa suuruus järjestyksessä:{result}')
+
+'''
+# Toinen tapa
+num_list = []
+
+while True:
+    user_input = input("Anna luku: ")
+    if user_input == "":
+        break
     try:
-        num = float(user_input)# muutetaan string floatiksi
+        num = float(user_input) # muutetaan string floatiksi
         num_list.append(num)
     except ValueError:
         print("Virheellinen syöte. Syötä kelvollinen luku.")
 
-num_list.sort(reverse = True)# Järjestetään nousevaan järjestykseen luvut
+num_list.sort(reverse = True) # Järjestetään nousevaan järjestykseen luvut
 
 result = num_list[:5]
 print(f'Tässä viisi suurinta numeroa suuruus järjestyksessä:{result}')
+'''
