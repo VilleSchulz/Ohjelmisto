e8f662e0c3662079295abedae44683bfe99d0288
Ville <villesc@gmail.com>
2023-09-08 09:09:12 +0300

    päivitetty heittolaskuri

---
+++ Moduuli6/Tehtävä2.py
@@ -0,0 +1,22 @@
+'''Muokkaa edellistä funktiota siten, että funktio saa parametrinaan nopan
+ tahkojen yhteismäärän. Muokatun funktion avulla voit heitellä esimerkiksi
+ 21-tahkoista roolipelinoppaa. Edellisestä tehtävästä poiketen nopan heittelyä
+ jatketaan pääohjelmassa kunnes saadaan nopan maksimisilmäluku, joka kysytään
+ käyttäjältä ohjelman suorituksen alussa.'''
+import random
+dice_maxnum = int(input('Anna arpakuution maksimi silmäluku:  '))
+dicenum = 0
+roll = 0
+while True:
+    dicenum = random.randint(1,dice_maxnum)
+    roll+=1
+    print(f'Heitto {roll}: {dicenum}')
+    if dice_maxnum == dicenum:
+        break
+print(f'Sait nopan maksimi silmäluvun: {dice_maxnum}')
+
+
+
+
+
+
