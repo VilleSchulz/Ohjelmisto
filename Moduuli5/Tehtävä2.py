a8b6e718f493633827f6cf05383dcac9060a6f39
Ville <villesc@gmail.com>
2023-09-08 11:11:30 +0300

    kommentit päivitetty

--- Moduuli5/Tehtävä2.py
+++ Moduuli5/Tehtävä2.py
@@ -9,12 +9,12 @@ while True:
     if user_input == "":
         break
     try:
-        num = float(user_input)
+        num = float(user_input)# muutetaan string floatiksi
         num_list.append(num)
     except ValueError:
         print("Virheellinen syöte. Syötä kelvollinen luku.")
 
-num_list.sort(reverse = True)
+num_list.sort(reverse = True)# Järjestetään nousevaan järjestykseen luvut
 
 result = num_list[:5]
 print(f'Tässä viisi suurinta numeroa suuruus järjestyksessä:{result}')
