d4895ac48b58d2340dffc38bfb7dd0e553b27b96
Ville <villesc@gmail.com>
2023-09-08 14:14:03 +0300

    päivitetty versio

--- Moduuli4/Tehtävä 3.py
+++ Moduuli4/Tehtävä 3.py
@@ -1,14 +1,32 @@
-#Kysyy lukua niin pitkään ennen kuin käyttäjä syöttää tyhjän merkkijonon
+#Kirjoita ohjelma, joka kysyy käyttäjältä lukuja siihen saakka, kunnes tämä syöttää tyhjän merkkijonon lopetusmerkiksi.
+# Lopuksi ohjelma tulostaa saaduista luvuista pienimmän ja suurimman.
 
-'''
-while True:
-    luku=input('Anna luku:')
-    if luku!="":
-        continue
-    else:
-        break
-'''
 print('Ohjelma tulostaa saaduista luvuista pienimmän ja suurimman')
+print('Jos annat jotain paitsi lukua - ohjelma lopettaa toimintansa \n')
+merkkien_jono = []
+luku = 0
+
+while True:
+    # kysyy käyttäjältä lukua
+    luku = input('Anna luku:')
+    # jos luku on erisuuri kuin tyhjä niin se listään listaan
+    if luku != "":
+        luku2 = int(luku)
+        merkkien_jono.append(luku2)
+    #jos luku on yhtäsuuri kuin "" ohjelma lopetetaan
+        if luku == "":
+        break
+
+
+print(f'{min(merkkien_jono)}: on pienin sinun luvuista \
+{max(merkkien_jono)}: on isoin sinun luvuista ')
+
+
+
+
+
+
+'''print('Ohjelma tulostaa saaduista luvuista pienimmän ja suurimman')
 print('Jos annat jotain paitsi lukua - ohjelma lopettaa toimintansa \n')
 
 var = input('Anna joku luku: ')
@@ -25,11 +43,10 @@ while True:
     except Exception:
         break
 
-print(f'''
-{min(merkkijen_jono)} - on pienin sinun luvuista
-{max(merkkijen_jono)} - on isoin sinun luvuista ''')
+print(f'{min[merkkijen_jono]} - on pienin sinun luvuista\
+{max[merkkijen_jono]} -on isoin sinun luvuista ')
 
-
+'''
 '''Joelin tapa tehdä(itselle muistiin ):
 num_list = []
 
