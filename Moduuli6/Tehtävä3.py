ebb8b793d453241b1c0949020bdf55654bc5604d
VilleSchulz <villemsc@metropolia.fi>
2023-09-12 13:06:27 +0300

    valmis

--- Moduuli6/Tehtävä3.py
+++ Moduuli6/Tehtävä3.py
@@ -2,3 +2,12 @@
  vastaavan litramäärän. Kirjoita pääohjelma, joka kysyy gallonamäärän käyttäjältä ja muuntaa sen litroiksi. Muunnos on
  tehtävä aliohjelmaa hyödyntäen. Muuntamista jatketaan siihen saakka, kunnes käyttäjä syöttää negatiivisen gallonamäärän.
 Yksi gallona on 3,785 litraa.'''
+def laskuri(litra):
+   litra =  litra * 3.785
+   return litra
+bensin = 0
+while bensin >= 0:
+    bensin = float(input("Anna bensiinin määrä gallonoina: "))
+    print(f'Bensiinin määrä litroina: {laskuri(bensin):.2f}')
+else:
+    print("Annoit negatiivisen luvun")
