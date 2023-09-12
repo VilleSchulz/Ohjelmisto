50f2e6d6aa08062add52ba9aa710d5b55ea94d8d
VilleSchulz <villemsc@metropolia.fi>
2023-09-12 13:33:41 +0300

    valmis

--- Moduuli6/Tehtävä4.py
+++ Moduuli6/Tehtävä4.py
@@ -1,2 +1,17 @@
 '''Kirjoita funktio, joka saa parametrinaan listan kokonaislukuja. Ohjelma palauttaa listassa olevien lukujen summan.
  Kirjoita testausta varten pääohjelma, jossa luot listan, kutsut funktiota ja tulostat sen palauttaman summan.'''
+
+def laskuri(summarum):
+    result =sum(summarum)
+    return result
+
+
+numero_list = []
+lista = input("Anna lukuja:")
+
+while lista != "":
+    numero_list.append(int(lista))
+    lista = input("Anna lukuja:")
+else:
+    lista == ""
+    print(laskuri(numero_list))
