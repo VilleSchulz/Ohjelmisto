88cff34dbfc79716050495d81d166d4dc8b8329b
Ville <villesc@gmail.com>
2023-09-16 13:18:49 +0300

    .

---
+++ Moduuli7/Tehtävä1.py
@@ -0,0 +1,23 @@
+'''Kirjoita ohjelma, joka kysyy käyttäjältä kuukauden numeron, jonka jälkeen ohjelma tulostaa sitä vastaavan vuodenajan
+ (kevät, kesä, syksy, talvi). Tallenna ohjelmassasi kuukausia vastaavat vuodenajat merkkijonoina
+ monikkotietorakenteeseen. Määritellään kukin vuodenaika kolmen kuukauden mittaiseksi siten, että joulukuu on
+ ensimmäinen talvikuukausi.'''
+
+months = ("tammmikuu", "helmikuu", "maaliskuu", "huhtikuu", "toukokuu", "kesäkuu", "heinäkuu", "elokuu", "syyskuu", "lokakuu", "marraskuu", "joulukuu")
+seasons = ("talvi", "kevät", "kesä", "syksy" )
+month_num = int(input("Anna kuukauden numero: "))
+
+if 1 <= month_num <= 12:
+    if month_num == 12 or month_num == 1 or month_num == 2:
+        season = 0
+    else:
+        season = month_num // 3
+
+    print(f'Kuukausi {months[month_num-1]} kuuluu vuodenaikaan {seasons[season]}')
+
+else:
+    print("Väärä kuukauden arvo")
+
+
+
+
