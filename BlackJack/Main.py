29bea8b537ec567c488a9f6f066ad88c65bc738a
Ville <villesc@gmail.com>
2023-10-17 12:42:18 +0300

    black jack testi

--- BlackJack/Main.py
+++ BlackJack/Main.py
@@ -13,40 +13,48 @@ def check(card):
 
 
 input("Welcome to Ville´s legal BlackJack! Press Enter to continue!")
-house_cards = []
-your_cards = []
+house_hand = []
+your_hand = []
 game_over = False
 #Game start
 house_card = deal()
-house_cards.append(house_card)
+house_hand.append(house_card)
 house_card = deal()
-house_cards.append(house_card)
-print(f"House's cards: {house_cards}")
-house_sum = sum(house_cards)
+house_hand.append(house_card)
+print(f"House's cards: {house_hand}")
+house_sum = sum(house_hand)
 print(f"House's total: {house_sum}")
-check(house_sum)
+if check(house_sum) == game_over:
+    print("You win!")
+    game_over = True
 input("Press Enter to deal a card!")
 your_card = deal()
-your_cards.append(your_card)
-print(f"Your cards: {your_cards}")
-your_sum = sum(your_cards)
+your_hand.append(your_card)
+your_card = deal()
+your_hand.append(your_card)
+print(f"Your cards: {your_hand}")
+your_sum = sum(your_hand)
 print(f"Your total: {your_sum}")
-check(your_sum)
+if check(your_sum) == game_over:
+    print("You lose")
+    game_over = True
 
-    while not game_over:
-        house_card = deal()
-        house_cards.append(house_card)
-        print(f"House's cards: {house_cards}")
-        house_sum = sum(house_cards)
-        print(f"House's total: {house_sum}")
-        check(house_sum)
-        input("Press Enter to deal a card!")
-        your_card = deal()
-        your_cards.append(your_card)
-        print(f"Your cards: {your_cards}")
-        your_sum = sum(your_cards)
-        print(f"Your total: {your_sum}")
-        check(your_sum)
+while not game_over:
+    house_card = deal()
+    house_hand.append(house_card)
+    print(f"House's cards: {house_hand}")
+    house_sum = sum(house_hand)
+    print(f"House's total: {house_sum}")
+    if check(house_sum) == game_over:
+        game_over = True
+    input("Press Enter to deal a card!")
+    your_card = deal()
+    your_hand.append(your_card)
+    print(f"Your cards: {your_hand}")
+    your_sum = sum(your_hand)
+    print(f"Your total: {your_sum}")
+    if check(your_sum) == game_over:
+        game_over = True
     if your_sum == 21 or your_sum > house_sum and your_sum<= 21:
         print("You win!")
 
