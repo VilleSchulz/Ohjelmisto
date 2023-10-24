53f902aa794583114f47be09fa864e5a31a133e4
Ville <villesc@gmail.com>
2023-10-24 19:48:08 +0300

    valmis

---
+++ Matikka/Matematiikka2_python1.py
@@ -0,0 +1,24 @@
+
+import numpy as np
+#Tehtävä 1
+a= 2.493
+b=0.911
+
+a_asteet = np.degrees(a)
+b_asteet = np.degrees(b)
+
+print(round(a_asteet, 1))
+print(round(b_asteet, 1))
+
+#Tehtävä2
+
+a= 137.7
+b=62.3
+
+a_radiaani = np.radians(a)
+b_radiaani = np.radians(b)
+
+print(round(a_radiaani, 3))
+print(round(b_radiaani, 3))
+
+#Tehtävä3
