12a16f714899b591a646880e5876faf40370d037
Ville <villesc@gmail.com>
2023-10-24 20:03:38 +0300

    valmis

--- Matikka/Matematiikka2_python1.py
+++ Matikka/Matematiikka2_python1.py
@@ -1,6 +1,6 @@
 
 import numpy as np
-#Tehtävä 1
+#Tehtävä1_1
 a= 2.493
 b=0.911
 
@@ -10,8 +10,8 @@ b_asteet = np.degrees(b)
 print(round(a_asteet, 1))
 print(round(b_asteet, 1))
 
-#Tehtävä2
-
+#Tehtävä1_2
+import numpy as np
 a= 137.7
 b=62.3
 
@@ -21,4 +21,19 @@ b_radiaani = np.radians(b)
 print(round(a_radiaani, 3))
 print(round(b_radiaani, 3))
 
-#Tehtävä3
+#Tehtävä1_3
+import numpy as np
+A = np.array([30, 45, 60,90,120,135,150,180,270,360])
+
+for i in A:
+    radian =np.radians(i)
+    print(round(radian,3))
+
+#Tehtävä2_1
+a=1.6
+b=2.3
+c = np.hypot(a,b)
+print(round(c,1))
+
+
+
