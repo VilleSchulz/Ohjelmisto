e5e18cac84a02c72965704b4621ea432bef7d515
VilleSchulz <villemsc@metropolia.fi>
2023-10-30 15:30:19 +0200

    valamis

--- Ohjelmisto2/Moduuli11/Tehtävä1.py
+++ Ohjelmisto2/Moduuli11/Tehtävä1.py
@@ -3,3 +3,21 @@ Kirjalla on lisäksi kirjoittaja ja sivumäärä, kun taas lehdellä on päätoi
 alustajat. Tee aliluokkiin metodi tulosta_tiedot, joka tudostaa kyseisen julkaisun kaikki tiedot. Luo pääohjelmassa
 julkaisut Aku Ankka (päätoimittaja Aki Hyyppä) ja Hytti n:o 6 (kirjailija Rosa Liksom, 200 sivua). Tulosta molempien
 julkaisujen kaikki tiedot toteuttamiesi metodien avulla.'''
+
+class Julkaisu:
+    def __init__(self,name):
+
+
+class Kirja(Julkaisu):
+    def __init__(self,writer,pages):
+        self.writer = writer
+        self.pages = pages
+    def tulosta_tiedot(self):
+        print(f"Kirjan nimi:{super().__init__(name)}, \n"
+              f"kirjan kirjoittoja: {self.writer} ja sivumäärä on {self.pages}")
+class Lehti(Julkaisu):
+    def __init__(self,päätoimittaja):
+        self.päätoimittaja = päätoimittaja
+    def tulosta_tiedot(self):
+        print(f"Lehden nimi on: {super()__init__(name)} ja päätoimittaja on {self.päätoimittaja}")
+
