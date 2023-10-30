7eb432ecdfec09d34d6d035004328033949a7828
Ville <villesc@gmail.com>
2023-10-30 10:01:45 +0200

    kesken

--- Ohjelmisto2/Moduuli10/Tehtävä4.py
+++ Ohjelmisto2/Moduuli10/Tehtävä4.py
@@ -11,3 +11,69 @@ kymmenen auton lista samaan tapaan kuin aiemmassa tehtävässä. Pääohjelma si
 toistorakenteessa tunti_kuluu-metodia, jonka jälkeen aina tarkistetaan kilpailu_ohi-metodin avulla, onko kilpailu ohi.
 Ajantasainen tilanne tulostetaan tulosta tilanne-metodin avulla kymmenen tunnin välein sekä kertaalleen sen jälkeen,
 kun kilpailu on päättynyt.'''
+
+
+
+
+import random
+
+class Kilpailu:
+    def __init__(self,nimi,kilometrimäärä,autolista):
+        self.nimi = nimi
+        self.kilometrimäärä = kilometrimäärä
+        self.autolista = autolista
+
+    def tunti_kuluu(self):
+        self.Auto.car.auto_kulje(1)
+
+    def tulosta_tilanne(self):
+        print(f'{"Rekisteritunnus ":<16}{"Huippunopeus ":<16}{"Nopeus (km/h)":<16}{"Ajettu km":<16}')
+        for car in self.auto_lista:
+            info = car.auto_info()
+            print(f'{info[0]:<16}{info[1]:<16}{info[2]:<16}{info[3]:<16}')
+class Auto():
+    def __init__(self,register,top_speed,speed = 0, km_amount = 0):
+        self.register = register
+        self.top_speed = top_speed
+        self.speed = speed
+        self.km_amount = km_amount
+
+
+
+    def auto_info(self):
+        return self.register,self.top_speed, self.speed,self.km_amount
+    def kiihdytä(self, difference):
+        if self.speed + difference > self.top_speed:
+            self.speed = self.top_speed
+        elif self.speed + difference < 0:
+            self.speed = 0
+        else:
+            self.speed += difference
+
+    def auto_kulje(self,time):
+        self.km_amount = self.km_amount + (time * self.speed)
+
+auto_lista = []
+for i in range(10):
+    new_car = Auto(f"ABC-{i+1}", random.randint(100,200))
+    auto_lista.append(new_car)
+
+'''while True:
+    for car in auto_lista:
+        car.kiihdytä(random.randint(-10,15))
+
+    if any(car.km_amount >= 10000 for car in auto_lista):
+            break'''
+
+
+
+kilpailu1 = Kilpailu(drag,100,auto_lista)
+
+kilpailu1.tunti_kuluu()
+
+
+
+
+
+
+
