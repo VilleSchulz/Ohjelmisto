3625aed0a8d216e1b90096ab9230cbd9206119b9
VilleSchulz <villemsc@metropolia.fi>
2023-11-01 12:29:52 +0200

    valamis

--- Ohjelmisto2/Moduuli11/Tehtävä2.py
+++ Ohjelmisto2/Moduuli11/Tehtävä2.py
@@ -11,27 +11,12 @@ import random
 
 
 class Auto():
-    def __init__(self,register,top_speed,speed = 0, km_amount = 0,capacity):
+    def __init__(self,register,top_speed,speed = 0, km_amount = 0):
         self.register = register
         self.top_speed = top_speed
         self.speed = speed
         self.km_amount = km_amount
-        super().__init__(capacity)
 
-
-class Sähköauto(Auto):
-    def __init__(self,capacity,register,top_speed):
-        self.capacity = capacity
-        super().__init__(register)
-        super().__init__(top_speed)
-
-
-
-class Polttomoottori(Auto):
-    def __init__(self,capacity,register,top_speed):
-        self.capacity = capacity
-        super().__init__(register)
-        super().__init__(top_speed)
     def auto_info(self):
         return self.register,self.top_speed, self.speed,self.km_amount
     def kiihdytä(self, difference):
@@ -44,6 +29,17 @@ class Polttomoottori(Auto):
 
     def auto_kulje(self,time):
         self.km_amount = self.km_amount + (time * self.speed)
+class Polttomoottori(Auto):
+    def __init__(self,register,top_speed,capacity_battery,speed):
+        self.capacity_battery = capacity_battery
+        super().__init__(register,top_speed,speed)
+
+
+class Sähköauto(Auto):
+    def __init__(self, register, top_speed, capacity_tank, speed):
+        self.capacity_tank = capacity_tank
+        super().__init__(register, top_speed, speed)
+
 
 auto_lista = []
 for i in range(10):
@@ -57,10 +53,17 @@ while True:
     if any(car.km_amount >= 10000 for car in auto_lista):
             break
 
-print(f'{"Rekisteritunnus ":<16}{"Huippunopeus ":<16}{"Nopeus (km/h)":<16}{"Ajettu km":<16}')
+'''print(f'{"Rekisteritunnus ":<16}{"Huippunopeus ":<16}{"Nopeus (km/h)":<16}{"Ajettu km":<16}')
 for car in auto_lista:
     info = car.auto_info()
     print(f'{info[0]:<16}{info[1]:<16}{info[2]:<16}{info[3]:<16}')
+'''
+auto1 = Sähköauto("ABC-15", 180, 52.5,180)
+auto2 =  Polttomoottori("ACD-123", 165,32.3,165)
 
+auto1.auto_kulje(3)
+auto2.auto_kulje(3)
+print(f"Sähköauton kulkema matka:{auto1.auto_info()[3]} km")
+print(f"Polttomoottori auton kulkema matka:{auto2.auto_info()[3]} km")
 
 
