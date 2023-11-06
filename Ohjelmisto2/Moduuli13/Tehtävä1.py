3be62472e2b5ebd274be1c330383a58c8223d9f3
VilleSchulz <villemsc@metropolia.fi>
2023-11-06 15:33:14 +0200

    valamis

---
+++ Ohjelmisto2/Moduuli13/Tehtävä1.py
@@ -0,0 +1,22 @@
+'''Toteuta Flask-taustapalvelu, joka ilmoittaa, onko parametrina saatu luku alkuluku vai ei. Hyödynnä toteutuksessa
+aiempaa tehtävää, jossa alkuluvun testaus tehtiin. Esimerkiksi lukua 31 vastaava GET-pyyntö annetaan muodossa:
+http://127.0.0.1:3000/alkuluku/31. Vastauksen on oltava muodossa: {"Number":31, "isPrime":true}.'''
+
+palavelin = Flask(__name__)
+@palavelin.route('/alkuluku/<num>')
+def get_root(num):
+    num = int(num)
+    if num / num %= 0 and num /1 %= 0:
+        prime = True
+    else:
+        prime = False
+    response_data = {"Number": num, "isPrime": prime}
+        return response_data
+
+#esimerkki query http://127.0.0.1:3000/kukkuu?name=ville
+
+
+
+
+if __name__ =='__main__':
+    palavelin.run(use_reloader=True,host='127.0.0.1',port=3000)
--- Ohjelmisto2/Moduuli13/Tuntiesimerkki.py
+++ Ohjelmisto2/Moduuli13/Tuntiesimerkki.py
@@ -1,8 +1,60 @@
-from flask import Flask
+from flask import Flask, request, Response
+import json
 
 palavelin = Flask(__name__)
 @palavelin.route('/')
 def get_root():
     return "Moro"
 
-palavelin.run()
+#esimerkki query http://127.0.0.1:3000/kukkuu?name=ville
+@palavelin.route('/kukkuu')
+def get_kukkuu():
+    #print(request.args)
+    name = request.args.get("name")
+    return f"haista paska {name}"
+
+#esimerkki query http://127.0.0.1:3000/kukkuu/ville
+@palavelin.route('/kukkuu/<name>')
+def get_kukkuu2(name):
+    #print(request.args)
+
+    return f"haista paska {name}"
+#esimerkki query http://127.0.0.1:3000/kukkuu/ville
+@palavelin.route('/kukkuu/<name>/<age>')
+def get_kukkuu3(name,age):
+    #print(request.args)
+
+    return f"haista paska {name}, {age}v"
+
+#esimerkki query http://127.0.0.1:3000/multiply/25
+#esimerkki response '{num:5, result: 25}'
+@palavelin.route('/multiply/<num>')
+def multiply(num):
+    # To Do: refactor code: create response
+
+    try:
+        num = int(num)
+    except ValueError:
+        response_data = {"msg": "input is not integer", "input_num": num}
+        # convert python dict to json format "manually"
+        # response_data = json.dumps(response_data)
+        response = Response(response=response_data, status=400, mimetype="application/json")
+        return response
+
+    if 0< num < 100:
+        result = num * num
+        response_data = {"msg":"Calculation done","input_num": num , "result": result}
+        return response_data
+    else:
+        response_data = {"msg": "input out of bounds", "input_num": num}
+        #convert python dict to json format "manually"
+        #response_data = json.dumps(response_data)
+        response = Response(response= response_data, status=400,mimetype="application/json")
+        return response
+
+
+if __name__ =='__main__':
+    palavelin.run(use_reloader=True,host='127.0.0.1',port=3000)
+
+
+
