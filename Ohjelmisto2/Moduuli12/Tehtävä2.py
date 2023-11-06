91dc67614464460698b31642fb4813d7e57696c8
VilleSchulz <villemsc@metropolia.fi>
2023-11-06 13:29:33 +0200

    valamis

--- Ohjelmisto2/Moduuli12/Tehtävä2.py
+++ Ohjelmisto2/Moduuli12/Tehtävä2.py
@@ -7,13 +7,23 @@ muunnettua Celsius-asteiksi.'''
 import requests
 end = False
 
+try:
+    while not end:
+        api_key = input("Anna api-key")
+        query = input("Anna paikkakunnan nimi jolta halua säätiedotteen!")
+        request =f"https://api.openweathermap.org/data/2.5/weather?q={query}&appid={api_key}&units=metric&lang=fi"
+        response_content = requests.get(request).json()
+        if int(response_content["cod"]) == 200:
+            temperature = response_content['main']['temp']
+            weather = response_content['weather'][0]['description']
+            print(f"Sää on {weather} ja lämpötila on {temperature} °C")
+            user_input = input("Haluatko hakea toisen kaupungin tiedot?(y/n)").lower()
+            if user_input == "n":
+                end = True
+        else:
+            print(f"error {response_content['cod']}")
+except requests.exceptions.RequestException as error:
+    print(error)
 
-while not end:
-    api_key = input("Anna api-key")
-    query = input("Anna paikkakunnan nimi jolta halua säätiedotteen!")
-    request =f"https://api.openweathermap.org/data/2.5/weather?q={query}&appid={api_key}&units=metric"
-    response_content = requests.get(request).json()
-    print(str(response_content["main"]["temp"])+ "°C")
-    user_input = input("Haluatko hakea toisen kaupungin tiedot?(y/n)").lower()
-    if user_input == "n":
-        end = True
+
+#jos haluat kelvineinä niin voi laittaa imperial queryyn tai sitte laskee K = °C + 273,15
