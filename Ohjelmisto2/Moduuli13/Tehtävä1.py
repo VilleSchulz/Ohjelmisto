3107d776dab1155914b956e5897609e9fc5aefaf
Ville <villesc@gmail.com>
2023-11-07 17:14:29 +0200

    valmis

--- Ohjelmisto2/Moduuli13/Tehtävä1.py
+++ Ohjelmisto2/Moduuli13/Tehtävä1.py
@@ -1,17 +1,29 @@
 '''Toteuta Flask-taustapalvelu, joka ilmoittaa, onko parametrina saatu luku alkuluku vai ei. Hyödynnä toteutuksessa
 aiempaa tehtävää, jossa alkuluvun testaus tehtiin. Esimerkiksi lukua 31 vastaava GET-pyyntö annetaan muodossa:
 http://127.0.0.1:3000/alkuluku/31. Vastauksen on oltava muodossa: {"Number":31, "isPrime":true}.'''
-from flask import Flask
+import json
+
+from flask import Flask, request,Response
 palavelin = Flask(__name__)
 @palavelin.route('/alkuluku/<num>')
 def get_root(num):
-    num = int(num)
-    if num % num == 0 and num % 1 == 0:
-        prime = True
+    try:
+        num = int(num)
+    except ValueError:
+        response_data = {"msg":"Input is not integer",
+                         "input_num": num
+                         }
+        status_code = 400
     else:
-        prime = False
-    response_data = {"Number": num, "isPrime": prime}
-    return response_data
+        if num % num == 0 and num % 1 == 0:
+            prime = True
+        else:
+            prime = False
+            response_data = {"Number": num, "isPrime": prime}
+            status_code = 200
+    response_data = json.dumps(response_data)
+    response = Response(response=response_data, status=status_code, mimetype= "application/json")
+    return response
 
 
 if __name__ =='__main__':
@@ -22,5 +34,4 @@ if __name__ =='__main__':
 
 
 
-if __name__ =='__main__':
-    palavelin.run(use_reloader=True,host='127.0.0.1',port=3000)
+
--- Ohjelmisto2/Moduuli13/Tuntiesimerkki.py
+++ Ohjelmisto2/Moduuli13/Tuntiesimerkki.py
@@ -1,60 +1,53 @@
 from flask import Flask, request, Response
 import json
 
-palavelin = Flask(__name__)
-@palavelin.route('/')
+app = Flask(__name__)
+# print(__name__)
+
+@app.route('/')
 def get_root():
-    return "Moro"
+    return "Moro maailma!"
 
-#esimerkki query http://127.0.0.1:3000/kukkuu?name=ville
-@palavelin.route('/kukkuu')
+# Example query: http://127.0.0.1:3000/kukkuu?name=Matti&age=25
+@app.route('/kukkuu')
 def get_kukkuu():
-    #print(request.args)
+    # print(request.args)
     name = request.args.get("name")
-    return f"haista paska {name}"
+    age = request.args.get("age")
+    return f"No kukkuu vaan, {name}, {age} vuotta!"
 
-#esimerkki query http://127.0.0.1:3000/kukkuu/ville
-@palavelin.route('/kukkuu/<name>')
-def get_kukkuu2(name):
-    #print(request.args)
+# Example query: http://127.0.0.1:3000/kukkuu/Matti/25
+@app.route('/kukkuu/<name>/<age>')
+def get_kukkuu_v2(name, age):
+    return f"No kukkuu vaan, {name}, {age} vuotta!"
 
-    return f"haista paska {name}"
-#esimerkki query http://127.0.0.1:3000/kukkuu/ville
-@palavelin.route('/kukkuu/<name>/<age>')
-def get_kukkuu3(name,age):
-    #print(request.args)
-
-    return f"haista paska {name}, {age}v"
-
-#esimerkki query http://127.0.0.1:3000/multiply/25
-#esimerkki response '{num:5, result: 25}'
-@palavelin.route('/multiply/<num>')
+# Example query: http://127.0.0.1:3000/multiply/5
+# Example response in json: '{input_num: 5, result: 25}'
+# input Num must be between 0-100
+@app.route('/multiply/<num>')
 def multiply(num):
-    # To Do: refactor code: create response
-
+    # TODO: refactor code: create response object and return it only once
     try:
         num = int(num)
     except ValueError:
-        response_data = {"msg": "input is not integer", "input_num": num}
+        response_data = {"msg": "Input is not an integer", "input_num": num}
         # convert python dict to json format "manually"
-        # response_data = json.dumps(response_data)
+        response_data = json.dumps(response_data)
+        # setting up a status code for response needs Response object to be created
+        response = Response(response=response_data, status=400, mimetype="application/json")
+        return response
+    if 0 < num < 100:
+        result = num * num
+        # response in dictinary format is translated to json automacically
+        response_data = {"msg": "Calculation done", "input_num": num, "result": result}
+        return response_data
+    else:
+        response_data = {"msg": "Input out of bounds", "input_num": num}
+        # convert python dict to json format "manually"
+        response_data = json.dumps(response_data)
+        # setting up a status code for response needs Response object to be created
         response = Response(response=response_data, status=400, mimetype="application/json")
         return response
 
-    if 0< num < 100:
-        result = num * num
-        response_data = {"msg":"Calculation done","input_num": num , "result": result}
-        return response_data
-    else:
-        response_data = {"msg": "input out of bounds", "input_num": num}
-        #convert python dict to json format "manually"
-        #response_data = json.dumps(response_data)
-        response = Response(response= response_data, status=400,mimetype="application/json")
-        return response
-
-
-if __name__ =='__main__':
-    palavelin.run(use_reloader=True,host='127.0.0.1',port=3000)
-
-
-
+if __name__ == '__main__':
+    app.run(use_reloader=True, host='127.0.0.1', port=3000)
