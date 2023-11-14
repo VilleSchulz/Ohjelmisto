02c2348bedb1ada0271bac9b94b4767ba57461bd
VilleSchulz <villemsc@metropolia.fi>
2023-11-14 14:56:38 +0200

    valamis

--- Ohjelmisto2/JavaScript/Moduuli2/1/Script/script.js
+++ Ohjelmisto2/JavaScript/Moduuli2/1/Script/script.js
@@ -1,7 +1,7 @@
 //Write a program that prompts the user for five numbers and prints them in
 // the reverse order they were entered. Print the result to the console.(2p)
 // Save the numbers to an array, then use for-loop to iterate in reverse order.
-// Do not use array.reverse() function.
+
 const num_list = [];
 for (i = 0; i < 5; i++) {
   let number = parseInt(prompt(`Give a number:${i+1}`));
