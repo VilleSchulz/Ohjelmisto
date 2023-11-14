e3b8397aa107640562b47d4289d21955b169306b
VilleSchulz <villemsc@metropolia.fi>
2023-11-14 13:13:01 +0200

    valamis

--- Ohjelmisto2/JavaScript/Moduuli1/8/Script/script.js
+++ Ohjelmisto2/JavaScript/Moduuli1/8/Script/script.js
@@ -1,15 +1,22 @@
-const start_year = parseInt(prompt('Anna aloitusvuosi'));
-const end_year = parseInt(prompt('Anna loppetusvuosi'));
-
+const start_year = parseInt(prompt('Give start year'));
+const end_year = parseInt(prompt('Give end year'));
 let yearList = '';
 
 if (start_year < end_year) {
   for (let i = 0; start_year + i <= end_year; i++) {
-    yearList += `<li>${start_year + i}</li>`;
+    const currentYear = start_year + i;
+    if ((currentYear % 4 === 0) && (currentYear % 100 !== 0) ||
+        (currentYear % 400 === 0)) {
+      yearList += `<li>${start_year + i}</li>`;
+    }
   }
 } else if (end_year < start_year) {
   for (let i = 0; end_year + i <= start_year; i++) {
-    yearList += `<li>${end_year + i}</li>`;
+    const currentYear = end_year + i;
+    if ((currentYear % 4 === 0) && (currentYear % 100 !== 0) ||
+        (currentYear % 400 === 0)) {
+      yearList += `<li>${end_year + i}</li>`;
+    }
   }
 }
 
