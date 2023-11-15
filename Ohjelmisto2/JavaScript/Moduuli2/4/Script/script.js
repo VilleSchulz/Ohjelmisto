0ddbe93c8986ace6961b9604ba7d7344dfb56c2a
VilleSchulz <villemsc@metropolia.fi>
2023-11-15 13:05:07 +0200

    valamis

--- Ohjelmisto2/JavaScript/Moduuli2/4/Script/script.js
+++ Ohjelmisto2/JavaScript/Moduuli2/4/Script/script.js
@@ -2,3 +2,22 @@
 // The given numbers are printed in the console from the largest to the
 // smallest. (2p)
 
+'use strict';
+const num_list = [];
+let count = 0;
+while (true) {
+  const num = parseInt(prompt('Give number! Hit 0 to cancel'));
+  num_list.push(num);
+  count + 1;
+  if (num === 0) {
+    break;
+  }
+}
+num_list.sort();
+num_list.reverse();
+
+for (let i = 0; count > i; i++) {
+  console.log(num_list[i]);
+
+}
+
