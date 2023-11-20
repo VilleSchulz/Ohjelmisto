be66acd325d8708340f285c4aeda998d725ce76f
VilleSchulz <villemsc@metropolia.fi>
2023-11-20 13:34:41 +0200

    valamis

--- Ohjelmisto2/JavaScript/Moduuli2/3/Script/script.js
+++ Ohjelmisto2/JavaScript/Moduuli2/3/Script/script.js
@@ -7,7 +7,7 @@ const targetElem = document.body;
 const olElement = document.createElement('ul');
 targetElem.append(olElement);
 
-for (i = 0; i < 6; i++) {
+for (let i = 0; i < 6; i++) {
   let dog_name = prompt(`Give name of the dog number ${i + 1}`);
   name_list.push(dog_name);
 }
@@ -17,7 +17,7 @@ for (i = 0; i < 6; i++) {
 
 name_list.sort();
 name_list.reverse();
-for (i = 0; i < 6; i++) {
+for (let i = 0; i < 6; i++) {
   const newLI = document.createElement('li');
   newLI.innerText = `${name_list[i]}`;
   olElement.append(newLI);
