7fbf44f887bb2e4eb6fb46b1ab5b07aa0e1fa8c9
VilleSchulz <villemsc@metropolia.fi>
2023-11-21 14:47:46 +0200

    valamis

---
+++ Ohjelmisto2/kertaus/03.py
@@ -0,0 +1,43 @@
+class Eläintarha:
+    # luokan konstruktori eli alustaja
+    def __init__(self, nimi):
+        self.nimi = nimi
+        self.eläimet = []
+
+    # luokan metodi
+    def lisää_eläin(self, eläin):
+        self.eläimet.append(eläin)
+
+    def listaa_eläimet(self):
+        for i in self.eläimet:
+            Eläin.tee_ääni(i)
+
+
+class Eläin():
+    def __init__(self, rotu, ääni):
+        self.rotu = rotu
+        self.ääni = ääni
+
+    def tee_ääni(self):
+        print(f'{self.rotu} {self.ääni}')
+
+
+class Lintu(Eläin):
+    def __init__(self, rotu, ääni, väri):
+        super().__init__(rotu, ääni)
+        #Eläin.__init__(self,rotu,ääni)
+        self.väri = väri
+
+
+eläin1 = Eläin('Leijona', 'Karjuu')
+eläin2 = Eläin('Kissa', 'Vinkuu')
+eläin3 = Lintu('Käki', 'Kukkuu', 'Ruskea')
+Eläintarha1 = Eläintarha('zoo')
+
+Eläintarha1.lisää_eläin(eläin1)
+eläin1.tee_ääni()
+
+Eläintarha1.lisää_eläin(eläin2)
+eläin2.tee_ääni()
+
+Eläintarha1.listaa_eläimet()
