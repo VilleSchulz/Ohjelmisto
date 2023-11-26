4196e531e93c5b11f3d8dc621eb43b37e0386bfe
VilleSchulz <villemsc@metropolia.fi>
2023-11-26 13:52:27 +0200

    valamis

---
+++ Ohjelmisto2/JavaScript/Moduuli3/2/Script/2.js
@@ -0,0 +1,20 @@
+//Open t2 folder in your IDE/editor. Add HTML by using createElement()
+// and appenChild mehtods. (2p)
+// Add the following HTML code to the element with id="target"
+//<li>First item</li>
+// <li>Second item</li>
+// <li>Third item</li>
+'use strict'
+
+const targetElement= document.getElementById('target')
+const newLI1 = document.createElement('li');
+const newLI2 = document.createElement('li');
+const newLI3 = document.createElement('li');
+targetElement.append(newLI1);
+newLI1.innerText = 'First item';
+targetElement.append(newLI2);
+newLI2.innerText = 'Second item';
+targetElement.append(newLI3);
+newLI3.innerText = 'Third item';
+
+
