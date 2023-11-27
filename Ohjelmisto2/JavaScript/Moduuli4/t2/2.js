e360f2f134f0a04697e49376cc9a4aa78b29d8b9
VilleSchulz <villemsc@metropolia.fi>
2023-11-27 16:27:09 +0200

    valamis

--- Ohjelmisto2/JavaScript/Moduuli4/t2/2.js
+++ Ohjelmisto2/JavaScript/Moduuli4/t2/2.js
@@ -15,22 +15,3 @@ button.addEventListener('click', async function (evt) {
         console.log(error.message);
     }
 })
-
-
-
-/**
-'use strict';
-const searchQuery = document.querySelector('#query');
-const button = document.querySelector('input[type="submit"]');
-
-button.addEventListener('click', async function (evt) {
-    evt.preventDefault();
-    const code = searchQuery.value;
-    try {
-        const response = await fetch(`https://api.tvmaze.com/search/shows?q=${code}`)
-        console.log(response);
-    } catch (error) {
-        console.log(error.message);
-    }
-})
-*/
