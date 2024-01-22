f64c879e6c66fc4428a14d79cd279ad207b70cdc
VilleSchulz <villemsc@metropolia.fi>
2024-01-22 21:11:19 +0200

    valamis

--- Ohjelmisto2/JavaScript/Moduuli4/t5/5.js
+++ Ohjelmisto2/JavaScript/Moduuli4/t5/5.js
@@ -1,8 +1,20 @@
 'use strict';
-const targetElement = document.body
+
+ async function joke (evt){
+   evt.preventDefault();
+   try{
+     await fetch()}
+catch (error){
+     console.log(error.message)
+}
+ }
+
+ const targetElement = document.body
 const container = document.createElement('div');
 targetElement.appendChild(container);
 const button = document.createElement('button');
 container.appendChild(button);
 button.innerText= 'Get me a JOKE!';
-button.style.scale='2';
+button.style.scale='2'
+
+button.addEventListener('click', joke);
