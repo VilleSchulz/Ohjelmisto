b3097fb110e8a2ce057961686113fbf3a7c17e24
VilleSchulz <villemsc@metropolia.fi>
2024-01-22 21:05:43 +0200

    valamis

--- Ohjelmisto2/JavaScript/Moduuli4/t5/5.js
+++ Ohjelmisto2/JavaScript/Moduuli4/t5/5.js
@@ -1,11 +1,6 @@
 'use strict';
 const targetElement = document.body
-targetElement.style.display = 'flex';
-targetElement.style.alignItems = 'center';
-targetElement.style.justifyContent = 'center';
-targetElement.style.flexDirection = 'column'
 const container = document.createElement('div');
-container.style.height= '10%';
 targetElement.appendChild(container);
 const button = document.createElement('button');
 container.appendChild(button);
