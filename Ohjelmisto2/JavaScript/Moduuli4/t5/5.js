00a2cbfdbc490dc4a8d6b459fdb494875adb64be
VilleSchulz <villemsc@metropolia.fi>
2024-01-23 09:48:31 +0200

    valamis

--- Ohjelmisto2/JavaScript/Moduuli4/t5/5.js
+++ Ohjelmisto2/JavaScript/Moduuli4/t5/5.js
@@ -1,20 +1,37 @@
 'use strict';
 
- async function joke (evt){
-   evt.preventDefault();
-   try{
-     await fetch()}
-catch (error){
-     console.log(error.message)
-}
- }
+async function joke(evt) {
+  evt.preventDefault();
+jokeTxt.innerText.replace('')
+  try {
+    const response = await fetch('https://api.chucknorris.io/jokes/random');
 
- const targetElement = document.body
+    const jsonData = await response.json();
+    console.log('Joke', jsonData.value);
+
+    jokeTxt.innerText = jsonData.value;
+  } catch {
+    (error);
+    {
+      console.log(error.message);
+    }
+  }
+}
+
+const targetElement = document.body;
 const container = document.createElement('div');
+container.classList.add('container');
 targetElement.appendChild(container);
 const button = document.createElement('button');
 container.appendChild(button);
-button.innerText= 'Get me a JOKE!';
-button.style.scale='2'
+button.innerText = 'Get me a JOKE!';
+button.style.scale = '2';
+const txtContainer = document.createElement('div');
+txtContainer.classList.add('txtcontainer');
+
+targetElement.appendChild(txtContainer)
+const jokeTxt = document.createElement('p');
+txtContainer.appendChild(jokeTxt);
+
 
 button.addEventListener('click', joke);
