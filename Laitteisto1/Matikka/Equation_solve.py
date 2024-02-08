fcd1fb5349e9dfb9492f6f9dc6d98d72af2dad0d
VilleSchulz <villemsc@metropolia.fi>
2024-02-08 11:17:02 +0200

    valamis

--- Laitteisto1/Matikka/Equation_solve.py
+++ Laitteisto1/Matikka/Equation_solve.py
@@ -1,9 +1,21 @@
 import numpy as np
-from sympy import symbols,solve
+from sympy import symbols, solve
+
+array = []
 x, y, z = symbols('x, y, z')
 
+equation_amount = int(input("Give me number of equations to solve \n"))
+input("Enter equation in this style :x+y+z-1\n"
+      "Press enter to continue.")
+for i in range(equation_amount):
+    equation = input(f"Give equation {i + 1}!")
+    array.append(equation)
 
-A3 = solve([x+y+z-3,
-           2*x+y+z-2,
-           x-2*y-2*z+2],[x,y,z])
-print(f"c) ={A3}\n")
+equations = array[:equation_amount]
+result = solve(equations, [x, y, z])
+
+
+'''A3 = solve([x + y + z - 3,
+            2 * x + y + z - 2,
+            x - 2 * y - 2 * z + 2], [x, y, z])'''
+print(result)
