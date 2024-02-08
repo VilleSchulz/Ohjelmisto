bf6f11ac13b47c64529d6f60ba3ba4754d231d72
VilleSchulz <villemsc@metropolia.fi>
2024-02-08 12:10:18 +0200

    lisätty error handler

--- Laitteisto1/Matikka/Automatic_equation_solve.py
+++ Laitteisto1/Matikka/Automatic_equation_solve.py
@@ -1,12 +1,18 @@
 import numpy as np
 from sympy import symbols, solve
 
+'''A3 = solve([x + y + z - 3,
+            2 * x + y + z - 2,
+            x - 2 * y - 2 * z + 2], [x, y, z])'''
+
+
+
 array = []
 x, y, z = symbols('x, y, z')
-equation_amount =1
 while True:
+    equation_amount = (input("Give me number of equations to solve \n"))
     try:
-        equation_amount = int(input("Give me number of equations to solve \n"))
+        equation_amount = int(equation_amount)
         break
     except ValueError:
         print(f"{equation_amount} is not a number")
@@ -19,10 +25,5 @@ for i in range(equation_amount):
 
 
 result = solve(array, [x, y, z])
-
-
-'''A3 = solve([x + y + z - 3,
-            2 * x + y + z - 2,
-            x - 2 * y - 2 * z + 2], [x, y, z])'''
 
 print(result)
