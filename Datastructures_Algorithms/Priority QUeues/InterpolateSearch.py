c86c54a0046bc4a1f834dd054f9c6a18a86cd362
VilleSchulz <villemsc@metropolia.fi>
2025-02-13 21:52:40 +0200

    agoritmit tehty eteenpäin

--- Datastructures_Algorithms/Priority QUeues/InterpolateSearch.py
+++ Datastructures_Algorithms/Priority QUeues/InterpolateSearch.py
@@ -1,26 +1,41 @@
-def interpolation_search(array, value):
-    start = 0
-    end = len(array) - 1
+def interpolation_search(array, value, start=None, end=None):
+    # Prepare the variables this way, so the user can call
+    # the function with just the array and value to be searched.
+    # If start and end are not defined, the search is conducted
+    # in the whole array.
+    if start is None:
+        start = 0
+    if end is None:
+        end = len(array) - 1
 
-    while start <= end and array[start] <= value <= array[end]:
-        # Estetään jakaminen nollalla
-        if array[start] == array[end]:
-            if array[start] == value:
-                return start
-            return None
+    # Calculate a midpoint
+    if array[start] == array[end]:  # Avoid division by zero
+        return start if array[start] == value else None
 
-        # Lasketaan interpolaatioindeksi
-        midpoint = start + ((end - start) * (value - array[start])) // (array[end] - array[start])
+    midpoint = start + int((end - start) * ((value - array[start]) / (array[end] - array[start])))
 
-        # Tarkistetaan, löytyikö arvo
-        if array[midpoint] == value:
-            return midpoint
-        elif array[midpoint] < value:
-            start = midpoint + 1  # Siirrytään oikealle
-        else:
-            end = midpoint - 1  # Siirrytään vasemmalle
+    # If the calculated midpoint is not within the search area, return
+    if midpoint < start or midpoint > end:
+        return None
 
-    return None  # Jos arvoa ei löydy
+    # If the value is found at the midpoint, return the index of midpoint
+    if array[midpoint] == value:
+        return midpoint
+
+    # If the value being searched is smaller than the one in the midpoint
+    # and there is at least one element left to the midpoint
+    if value < array[midpoint] and midpoint >= start + 1:
+        # Perform the search on the left part
+        return interpolation_search(array, value, start=start, end=midpoint - 1)
+
+    # If the value being searched is bigger than the one in the midpoint
+    # and there is at least one element right to the midpoint
+    if value > array[midpoint] and midpoint <= end - 1:
+        # Perform the search on the right part
+        return interpolation_search(array, value, start=midpoint + 1, end=end)
+
+    return None
+
 
 # Testidata
 array = [0, 5, 8, 11, 14, 17, 29, 31, 31, 35, 39, 40, 47, 61, 68, 78, 85, 88, 95, 98]
